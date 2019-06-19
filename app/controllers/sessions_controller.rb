1e33fff222fac210cf00c5b46c648296ec1ec12f
fut0k0 <cccabell@icloud.com>
2019-06-18 17:58:44 -0700

    added sessions login & logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,16 +6,22 @@ class SessionsController < ApplicationController
 	
 	def create
 		@user = User.find_by(name: params[:name])
+		
 		if @user
 			session[:user_id] = @user.id
 			
+			#flash[:message] = "Welcome #{@user.name}"
 			redirect_to books_path
 		else
-			
+			redirect_to login_path
 		end
 	end
 
 	def destroy
+		session.delete :user_id
 		
+		byebug
+		
+		redirect_to login_path
 	end
 end
