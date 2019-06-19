850a444842c390137e325bcdab22d2a40fea7894
jenshaw86 <bloo.j.shaw@gmail.com>
2019-06-19 15:06:05 -0700

    debugged transaction

--- db/migrate/20190619185427_add_open_to_transactions.rb
+++ db/migrate/20190619185427_add_open_to_transactions.rb
@@ -1,4 +1,4 @@
-class AddColumnToTransactions < ActiveRecord::Migration[5.2]
+class AddOpenToTransactions < ActiveRecord::Migration[5.2]
   def change
     add_column :transactions, :open, :integer
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_19_213343) do
+ActiveRecord::Schema.define(version: 2019_06_19_185427) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
