c749557194fdb1fc92a10e0fc5fd125c45e8688c
fut0k0 <cccabell@icloud.com>
2019-06-20 11:32:31 -0700

    added new book form and route

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,20 @@
 class BooksController < ApplicationController
-   before_action :logged_in
+    before_action :logged_in
+  
+    def index
+      @books = Book.all
+    end
+  
+    def show
+      @book = Book.find(params[:id])
+      @transaction = Transaction.new
+    end
+  
+    def new
+	    @book = Book.new
+	  end
+	
+	  def create
+	  end
    
-   def index
-    @books = Book.all
-   end
-   
-   def show
-    @book = Book.find(params[:id])
-    @transaction = Transaction.new
-   end
 end
